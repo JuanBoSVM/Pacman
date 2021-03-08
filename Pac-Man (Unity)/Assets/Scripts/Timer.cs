a38a8b52957e63e72ed3032e31b2f48614fd1afe
Luis Eduardo Juárez Lagarde <ljuarezlagarde@gmail.com>
2021-03-08 02:40:52 -0600

    Timer & win condition, credits and ghost animators

---
+++ Pac-Man (Unity)/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timeText;
+    public float time = 0.0f;
+    public PacMan paco;
+    public GameObject VictoryScreen;
+    public GameObject GameOverScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time <=  0)
+        {
+            VictoryScreen.SetActive(true);
+        }
+
+        if (!paco.anim.GetBool("Alive"))
+        {
+            GameOverScreen.SetActive(true);
+        }
+
+        if (paco.anim.GetBool("Alive") && time > 0)
+        {
+            time -= Time.deltaTime;
+            timeText.text = "" + time.ToString("f0");
+        }
+    }
+}
